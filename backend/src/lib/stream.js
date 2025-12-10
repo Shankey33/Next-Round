0ec9746500994d7e2a9a15fc144e06fe705ade21
Shankey33 <aadidwivedigwl@gmail.com>
2025-12-10 11:07:07 +0530

    Synchronization between clerk - db - stream completed, configured deployment acc to it as well

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,28 @@
+import {StreamChat} from "stream-chat";
+import {ENV} from "../env.js";
+
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSectet = ENV.STREAM_API_SECRET;
+
+if(!apiKey || !apiSecret) console.log("STREAM_API_KEY and/or STREAM_API_SECRET is/are missing");
+
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+
+export const upsertSteamUser = async(userData) => {
+    try{
+        await chatClient.upsertUser(userData);
+        console.log("Stream user upserted successfully!")
+    } catch(err){
+        console.log("Error setting up user for stream chat client!, ", err)
+    }
+}
+
+export const deleteSteamUser = async(userId) => {
+    try{
+        await chatClient.deleteUSer([userData]);
+        console.log("Stream User deleted successfully!");
+    } catch(err){
+        console.log("Error deleting user for stream chat service!, ", err)
+    }
+}
