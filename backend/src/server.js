6bfbcb58749bd61b901814fdae9bc2fe5007658d
Aadi <aadidwivedigwl@gmail.com>
2025-12-25 04:41:19 +0530

    fixed inngest api call blockage

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,12 +13,14 @@ import chatRoutes from "./routes/chatRoutes.js";
 import sessionRoutes from "./routes/sessionRoutes.js";
 
 const app = express();
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
 app.use(express.json())
 app.use(cors({origin:ENV.CLIENT_URL, credentials:true}))
 
 app.use(clerkMiddleware()); // This adds auth field to req objects: req.auth()
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 
